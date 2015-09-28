6d6d5d1498fe13c11e22cb7f6a9315e6541baa08
Marco Pegoraro <marco.pegoraro@gmail.com>
2015-09-28 16:49:12 +0200

    simple link in the real world

--- demo/server.js
+++ demo/server.js
@@ -2,17 +2,15 @@ var path = require('path');
 var fs = require('fs');
 var express = require('express');
 var cheerio = require('cheerio');
+var Mustache = require('mustache');
 
 var app = express();
 
-// veeeery slow and painful loading experience (with chuck norris)
+// delayed HTML apis
+app.get('/ajax-link.html', serveWithDelay('ajax-link.html', 0));
 app.get('/d001b.html', serveWithDelay('d001b.html', 20000));
+app.get('/d003b.html', serveWithDelay('ajax-link.html', 3000));
 
-// delayed ajax requests
-app.get('/p2.html', serveWithDelay('ajax-link.html', 1));
-app.get('/p2-fast.html', serveWithDelay('ajax-link.html', 800));
-app.get('/p2-slow.html', serveWithDelay('ajax-link.html', 3000));
-app.get('/p2-death.html', serveWithDelay('ajax-link.html', 10000));
 
 // static resources
 app.use(express.static(path.join(__dirname, 'node_modules')));
@@ -29,13 +27,31 @@ function serveWithDelay(uri, delay) {
     return function(req, res, next) {
         fs.readFile(path.join(__dirname, 'pages', uri), 'UTF-8', function(err, content) {
             setTimeout(function() {
+                
+                // parse placeholders
+                function send(content) {
+                    res.send(Mustache.render(content, {
+                        'random-gif' : randomGif()
+                    }));
+                }
+
                 if (req.xhr) {
                     var $ = cheerio.load(content);
-                    res.send($('#content').html());
+                    send($('#content').html());
                 } else {
-                    res.send(content);
+                    send(content);
                 }
             }, delay);
         });
     };
+}
+
+
+function randomGif() {
+    var gifs = fs.readdirSync(path.join(__dirname, 'pages', 'gifs'));
+    return ['gifs', randomItem(gifs)].join('/');
+}
+
+function randomItem(list) {
+    return list[Math.floor(Math.random()*list.length)];
 }
