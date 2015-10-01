bdf8a280a946dcb1aa6f54d95470398afb229e7d
marcopeg <marco.pegoraro@gmail.com>
2015-10-01 12:43:21 +0200

    improvements

--- demo/pages/index.js
+++ demo/pages/index.js
@@ -5,3 +5,14 @@ $(document).on('keyup', function(e) {
         $('#content').html('');
     };
 });
+
+function loadingFadeIn() {
+	$('#loading-curtain').addClass('fading-active fading-visible');
+}
+
+function loadingFadeOut() {
+	$('#loading-curtain').removeClass('fading-visible');
+    setTimeout(function() {
+        $('#loading-curtain').removeClass('fading-active');
+    }, 200);
+}
